904a7928bf1e5e990438501f130f3afbf480496a
martin <m.stiehr@gmail.com>
2023-07-31 14:11:50 +0200

    init spring boot

---
+++ src/test/java/de/mstiehr/polar/catalogservice/CatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.mstiehr.polar.catalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
